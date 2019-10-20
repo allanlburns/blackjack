3d3a01a64d6156efb7ff8382c2986c33bf168674
Allan Burns <allanlburns@yahoo.com>
2019-10-20 11:20:27 -0400

    Tested instantiating player classes and adding player's name.

--- blackjack.py
+++ blackjack.py
@@ -1,11 +1,28 @@
-class game():
+class Game():
+    def __init__(self):
+        playing = True
+
+class Card():
     pass
 
-class card():
+class Player():
+    def __init__(self, name):
+        self.name = name
+        self.score = 0
+        self.hand = []
+
+class Deck():
     pass
 
-class player():
-    pass
+playing = True
 
-class deck():
-    pass
+while playing:
+    player_name = input("Please type your name ")
+
+    player = Player(name=player_name)
+    dealer = Player(name='Dealer')
+
+    print(f'{dealer.name} has {dealer.hand}')
+    print(f'{player.name} has {player.hand}')
+
+    playing = False
