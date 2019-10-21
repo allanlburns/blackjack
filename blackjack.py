a5aa4e52bc624f741577ca06908c3fd7023229f3
Allan Burns <allanlburns@yahoo.com>
2019-10-20 20:16:51 -0400

    Implemented win/lose/bust logic.

--- blackjack.py
+++ blackjack.py
@@ -5,10 +5,24 @@ class Game():
     def __init__(self):
         self.playing = True
 
+    def check_for_bust(self, player):
+        if player.total > 21:
+            print(f"{player.name} BUSTED! {player.total}")
+            return True
+
+    def compare_results(player1, player2):
+        if player1.total == player2.total:
+            return f"It's a tie: Your score is {player1.score} ; Dealer's score is {player2.score}"
+        elif player1.total > player2.total:
+            return f"You win! : Your score is {player1.score} ; Dealer's score is {player2.score}"
+        elif player1.total < player2.total:
+            return f"Dealer wins! : Your score is {player1.score} ; Dealer's score is {player2.score}"
+
 class Card():
     def __init__(self, suit, value):
         self.suit = suit
         self.value = value
+
     def __str__(self):
         return f'{self.value} of {self.suit} '
 
@@ -17,8 +31,10 @@ class Player():
         self.name = name
         self.total = 0
         self.hand = []
+        self.play = None
+        self.busted = False
 
-    def score(self):
+    def calc_score(self):
         for card in self.hand:
             if card.value == 'A' and self.hand.count('A') < 1:
                 self.total += 11
@@ -29,6 +45,30 @@ class Player():
             else:
                 self.total += int(card.value)
         return self.total
+
+    def set_play(self, choice):
+        if choice == 'hit':
+            deck.deal_card(self)
+            player.play = "Hit"
+        elif choice == 'stay':
+            player.play = "Stay"
+
+    # def display_cards(self):
+    #     if self.name == 'dealer':
+    #         # Display dealer's second card onward:
+    #         print('\n', f"{dealer.name}'s hand:", end=" ")
+    #         print("? of ?,", end=' ')
+    #         for card in dealer.hand[1:]:
+    #             print(card, end='')
+    #
+    #     else:
+    #         # Display player's cards and total:
+    #         print('\n', f"{player.name}'s hand after dealing cards:", end=" ")
+    #         for card in player.hand:
+    #             print(card, end='')
+    #
+    #         print('\n', f"{player.name}'s total = {player.score()}")
+
 
 class Deck():
     def __init__(self):
@@ -44,6 +84,10 @@ class Deck():
         for num in range(0, quantity):
             player.hand.append(self.cards.pop(randint(0, len(self.cards) - 1)))
 
+################################################################################
+# Main game loop
+################################################################################
+
 game = Game()
 
 player_name = input("Please type your name ")
@@ -53,31 +97,103 @@ while game.playing:
     player = Player(name=player_name)
     dealer = Player(name='Dealer')
 
-    # Display dealer's and player's cards:
-
-    # Placeholder for actual game logic
-    print("Pretend we played a game...", '\n')
+    # Instantiate deck and deal first two cards to dealer and player
     deck = Deck()
-
 
     deck.deal_card(player, 2)
     deck.deal_card(dealer, 2)
 
-    print('\n', f"{player.name}'s hand after dealing cards:", end=" ")
+
+    # Show player's cards
+    print(f"{player.name}'s hand after dealing cards: ", '\n')
     for card in player.hand:
-        print(card, end='')
-
+        print(card)
     # display player's current total
-    print('\n', f"{player.name}'s total = {player.score()}")
+    print(f"{player.name}'s total = {player.calc_score()}")
 
-    print('\n', f"{dealer.name}'s hand:", end=" ")
+    # Show dealer's cards from secont card on:
+    print(f"{dealer.name}'s hand:", '\n')
     print("? of ?,", end=' ')
     for card in dealer.hand[1:]:
-        print(card, end='')
+        print(card)
 
-    # display dealer's current total
-    print('\n', f"Dealer's total = {dealer.score()}")
+    print('\n')
+    #  calculate dealer's current total
+    dealer.calc_score()
 
+    time.sleep(1)
+
+
+
+
+    # Loop through and allow player and dealer to hit or stay until one busts or both stay. Check for bust with each pass.
+
+    # while player.play != 'Stay' and dealer.play != 'Stay':
+    #     choice = input("Would you like to hit or stay? (Hit/Stay)")
+    #     player.set_play(choice)
+    #
+    #     if dealer.total > 16:
+    #         dealer.set_play('Stay')
+    #     else:
+    #         dealer.set_play('Hit')
+    #
+    #     if player.total > 21 or dealer.total > 21:
+    #         break
+
+    while True:
+        # Player: hit or stay?
+        choice = input("Would you like to hit or stay? (Hit/Stay)")
+        if choice == 'hit':
+            deck.deal_card(player)
+            player.score()
+        if player.total > 21:
+            print("You BUSTED!!! ")
+            break
+        elif choice == 'stay':
+            player.play = 'stay'
+        else:
+            print("Please select a valid play. ")
+            continue
+
+        time.sleep(1)
+
+        # display player's hand and score:
+        print(f"{player.name}'s hand after dealing cards:", '\n')
+        for card in player.hand:
+            print(card, '\n')
+        # display player's current total
+        print('\n', f"{player.name}'s total = {player.total}")
+
+        # dealer: hit or stay?
+        if dealer.total <= 16:
+            print("Dealer hits.")
+            deck.deal_card(player)
+            dealer.score()
+            dealer.play = 'stay'
+        if player.total > 21:
+            print("Dealer BUSTED!!! ")
+            break
+        elif dealer.total > 16:
+            dealer.play = 'stay'
+
+        print(f"{dealer.name}'s hand:", '\n')
+        print("? of ?,", end=' ')
+        for card in dealer.hand[1:]:
+            print(card, '\n')
+
+        if dealer.play == 'stay' and player.play == 'stay':
+            break
+
+    if player.busted:
+        print(f"You busted! Dealer wins! Your score is {player.total} ; Dealer's total is {dealer.total}")
+    elif dealer.busted:
+        print(f"You win! Dealer busted! Your total is {player.total} ; Dealer's total is {dealer.total}")
+    elif player.total == dealer.total:
+        print(f"It's a tie: Your total is {player.total} ; Dealer's total is {dealer.total}")
+    elif player.total > dealer.total:
+        print(f"You win! : Your total is {player.total} ; Dealer's total is {dealer.total}")
+    elif player.total < dealer.total:
+        print(f"Dealer wins! : Your total is {player.total} ; Dealer's total is {dealer.total}")
 
     # Is it time to break out of the main game loop?
     play_again = input("Would you like to play again? (y/n) ")
@@ -86,7 +202,3 @@ while game.playing:
         game.playing = False
         print("Thanks for playing!")
         time.sleep(2)
-
-
-# TODO:
-# handling aces: if aces in hand and total over 21, subtract 10 times the number of aces.
